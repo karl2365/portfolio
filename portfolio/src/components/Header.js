5029f44c35c6fcf503753d97f4ea0c0fbc1acd50
karl2365 <karlbeksa@gmail.com>
2020-06-02 13:02:51 -0400

    initial commit

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+      <div className="App">
+          <h1>This is my header</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
